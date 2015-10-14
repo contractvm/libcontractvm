92c62883b06ac54a32663aa805d94ed85e0949e4
Davide Gessa <gessadavide@gmail.com>
2015-10-14 13:00:20 +0200

    release 0.6

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(name='libcontractvm',
-	version='0.1',
-	description='ContractVM library',
+	version='0.6',
+	description='Contractvm client library',
 	author='Davide Gessa',
 	setup_requires='setuptools',
 	author_email='gessadavide@gmail.com',
